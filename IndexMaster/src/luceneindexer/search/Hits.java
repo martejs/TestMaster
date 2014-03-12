25c4191a185f67c47ca210cecd3fbdcf33b6f7a4
Mathilde Oftedal <mathilde.oftedal@gmail.com>
2014-03-12 14:50:57 +0100

    Prøvd å lage en egen termfreq

--- IndexMaster/src/luceneindexer/search/Hits.java
+++ IndexMaster/src/luceneindexer/search/Hits.java
@@ -1,19 +1,26 @@
 package luceneindexer.search;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 
 
 public class Hits {
 	
 	private HashMap<String, Integer> terms = new HashMap<String, Integer>();
-	private int occurrences = 1;
+	private int occurrences = 0;
 	
 	public void setTerms(String term){
-		if(this.terms.containsKey(term)){
-			terms.put(term, occurrences++);
+		if(!this.terms.containsKey(term)){
+			this.occurrences = 1;
+			System.out.println("f¿rste gang: " + term);
+			System.out.println(this.occurrences);
+			terms.put(term, this.occurrences);
 		}
 		else{
-			terms.put(term, occurrences);
+			System.out.println("igjen:" + term);
+			this.occurrences++;
+			System.out.println(this.occurrences);
+			terms.put(term, this.occurrences);
 		}
 	}
 	
