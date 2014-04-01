9f49c7cbb84e9f572a309780e66f096be2e7af94
Mathilde Oftedal <mathilde.oftedal@gmail.com>
2014-04-01 09:44:02 +0200

    Mye rart..

--- IndexMaster/src/luceneindexer/javascript/Servlet.java
+++ IndexMaster/src/luceneindexer/javascript/Servlet.java
@@ -15,30 +15,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Servlet {
 
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+			throws ServletException, IOException {
 
-     // 1. get received JSON data from request
-        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-        String json = "";
-        if(br != null){
-            json = br.readLine();
-        }
- 
-        // 2. initiate jackson mapper
-        ObjectMapper mapper = new ObjectMapper();
- 
-        // 3. Convert received JSON to Article
-        String string = mapper.readValue(json, String.class);
-        SearchDBpedia searchDB = new SearchDBpedia(string);
- 
-        // 4. Set response type to JSON
-        response.setContentType("application/json");            
- 
- 
-        // 6. Send List<Article> as JSON to client
-//        mapper.writeValue(response.getOutputStream(), string);
+		response.setHeader("Access-Control-Allow-Origin","Servlet.java");
+		
+		// 1. get received JSON data from request
+		System.out.println("Kalles fra javascript");
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String json = "";
+		if(br != null){
+			json = br.readLine();
+		}
 
-        
+		// 2. initiate jackson mapper
+		ObjectMapper mapper = new ObjectMapper();
+
+		// 3. Convert received JSON to Article
+		String string = mapper.readValue(json, String.class);
+		SearchDBpedia searchDB = new SearchDBpedia(string);
+
+		// 4. Set response type to JSON
+		response.setContentType("application/json");            
+
+
+		// 6. Send List<Article> as JSON to client
+		//        mapper.writeValue(response.getOutputStream(), string);
+
+
 	}
-	
+
 }
--- IndexMaster/src/luceneindexer/javascript/mainPage.js
+++ IndexMaster/src/luceneindexer/javascript/mainPage.js
@@ -18,19 +18,37 @@ $('#searchBtn').click(function(){
 			'rows':100
 	}
 
+	//ENTEN DENNE TYPEN
+//	var invocation = new XMLHttpRequest();
+//	var url = 'Servlet.java';
+//	var body = '<?xml version="1.0"?><query>' + query + '</query>';
+//
+//	function callOtherDomain() {
+//		if(invocation) {    
+//			console.log("inni callOtherDomain");
+//			invocation.open('POST', url, true);
+//			invocation.setRequestHeader('Query', 'query');
+//			invocation.setRequestHeader('Content-Type', 'application/xml');
+//			invocation.onreadystatechange = handler;
+//			invocation.send(body); 
+//		}
+//	}
 
+	//ELLER DENNE...
 	$.ajax({
-        url: "Servlet.java",
-        type: 'POST',
-        dataType: 'json',
-        data: JSON.stringify(query),
-        contentType: 'application/json',
-        mimeType: 'application/json',
- 
-        success: function (data) {
-        	console.log(data);
-        },
-    });
+	'url': "Servlet.java",
+	'type': 'POST',
+	'dataType': 'json',
+	'data': JSON.stringify(query),
+	'contentType': 'application/json',
+	'mimeType': 'application/json',
+//	'Access-Control-Allow-Credentials': "true",
+	"origin": "Servlet.java",
+//	'permissions': "Servlet.java",
+	'success': function (data) {
+	console.log(data);
+	},
+	});
 
 	/*$.ajax({
 		'url': 'http://129.241.111.168:8983/solr/SanFrancisco/select',
@@ -113,21 +131,21 @@ $('#searchBtn').click(function(){
 		'jsonp':'json.wrf'
 		});*/
 
-	$.ajax({
-		'url': 'http://129.241.111.168:8983/solr/WorldGEOUpcoming/select',
-		'data': obj,
-		'success': function(data) {
-			console.log("6");
-			for(var i=0;i<data.response.docs.length;i++){
-				var URL = data.response.docs[i].url_s;
-				$('#searchResult').append('<img src="' + URL + '">');
-				console.log(data.response.docs[i]);
-			}
+//	$.ajax({
+//	'url': 'http://129.241.111.168:8983/solr/WorldGEOUpcoming/select',
+//	'data': obj,
+//	'success': function(data) {
+//	console.log("6");
+//	for(var i=0;i<data.response.docs.length;i++){
+//	var URL = data.response.docs[i].url_s;
+//	$('#searchResult').append('<img src="' + URL + '">');
+//	console.log(data.response.docs[i]);
+//	}
 
-		},
-		'dataType':'jsonp',
-		'jsonp':'json.wrf'
-	});
+//	},
+//	'dataType':'jsonp',
+//	'jsonp':'json.wrf'
+//	});
 
 //	$.ajax({
 //	'url': 'http://129.241.111.168:8983/solr/WorldTiles/select',
