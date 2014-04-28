76182a784a9aede144f66470853304b0b5d41a42
Mathilde Oftedal <mathilde.oftedal@gmail.com>
2014-04-28 18:35:50 +0200

    endret noe

--- IndexMaster/src/luceneindexer/javascript/Servlet.java
+++ IndexMaster/src/luceneindexer/javascript/Servlet.java
@@ -1,8 +1,6 @@
 package luceneindexer.javascript;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -10,28 +8,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import luceneindexer.search.SearchDBpedia;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Servlet extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String q = req.getParameter("q");
+		PrintWriter out = resp.getWriter();
 
-		SearchDBpedia searchDB = new SearchDBpedia(q, 3);
-
+		out.println("<html>");
+		out.println("<body>");
+		out.println("The paramter q was \"" + q + "\".");
+		out.println("</body>");
+		out.println("</html>");
 
 	}
 
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
-		String field = req.getParameter("field");
+		
+		String[] response = req.getParameterValues("method");
+		
+		String method = response[0];
+		String field = response[1];
+
+		SearchDBpedia searchDB = new SearchDBpedia(field, 3);
+
 		PrintWriter out = resp.getWriter();
 
 		out.println("<html>");
